47420f01e46f20d0183fdc90811ad283667e2941
Samuel Delacrausaz <samuel_delacrausaz@teilnehmer.cdemy.de>
2024-11-18 09:53:58 +0100

    x

--- obj/Debug/net8.0/MarysCandyShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/MarysCandyShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarysCandyShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+462a2277f1a51ac35a6320e1a19ce4c53d627005")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e4acd0c44479786dd742c276cb6dd538f4be97")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarysCandyShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarysCandyShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
